d09fec75cd2413938c2e1a9cd0463caf6f8fc2f5
Jordan Yaker <jordan.yaker@stuzo.com>
2022-05-19 14:29:34 -0400

    Added Delta Lake to the project.

--- notebooks/lib/spark.py
+++ notebooks/lib/spark.py
@@ -2,8 +2,10 @@ import os
 
 from typing import Dict, List
 
+from delta import configure_spark_with_delta_pip
 from pyspark import SparkConf
 from pyspark.sql import SparkSession
+
 
 os.environ["PYARROW_IGNORE_TIMEZONE"] = "1"
 
@@ -12,14 +14,17 @@ SESSION_DEFAULT_CONF = {
     "spark.sql.adaptive.coalescePartitions.minPartitionSize": "2",
     "spark.sql.adaptive.coalescePartitions.initialPartitionNum": "2",
     "spark.sql.adaptive.coalescePartitions.enabled": "true",
-    "fs.s3a.aws.credentials.provider": "com.amazonaws.auth.DefaultAWSCredentialsProviderChain"
+    "fs.s3a.aws.credentials.provider": "com.amazonaws.auth.DefaultAWSCredentialsProviderChain",
+    "spark.sql.extensions": "io.delta.sql.DeltaSparkSessionExtension",
+    "spark.sql.catalog.spark_catalog": "org.apache.spark.sql.delta.catalog.DeltaCatalog",
 }
 
 SPARK_DEFAULT_PACKAGES = [
-    "org.postgresql:postgresql:42.3.5",
+    "com.databricks:spark-xml_2.12:0.14.0",
+    "io.delta:delta-core_2.12:1.2.1",
     "org.apache.hadoop:hadoop-aws:3.3.1",
-    "com.databricks:spark-xml_2.12:0.14.0"
-
+    "org.apache.spark:spark-streaming-kinesis-asl_2.12:3.2.1",
+    "org.postgresql:postgresql:42.3.5",
 ]
 
 def get_spark_session(config: Dict[str, str] = None, driver = "spark://driver:7077",
@@ -43,6 +48,8 @@ def get_spark_session(config: Dict[str, str] = None, driver = "spark://driver:70
     builder = SparkSession.builder \
                           .master(driver) \
                           .appName(name)
+
+    builder = configure_spark_with_delta_pip(builder)
 
     for k, v in conf.items():
         builder = builder.config(k, v)
